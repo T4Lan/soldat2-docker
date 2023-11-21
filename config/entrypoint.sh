b16806d72c734b7b2f301e50a095d03c904c80f9
Matias Micheltorena <m@matiargs.com>
2023-11-21 20:23:23 -0300

    update file structure

--- config/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd soldat2
-
-sed -i "s|RCON_PASSWORD|${RCON_PASSWORD}|g" autoconfig.ini
-sed -i "s|SERVER_NAME|${SERVER_NAME}|g" autoconfig.ini
-sed -i "s|SERVER_GREET_MESSAGE|${SERVER_GREET_MESSAGE}|g" autoconfig.ini
-
-exec "$@"
---
+++ etc/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+mkdir -p "${GAMEDIR}" || true
+
+if [ ! -f "${GAMEDIR}/soldat2" ]; then
+    echo "Server not found, unpacking it"
+    rm -rf ./* # clear server files
+    tar -xf "${TMPDIR}/soldat2/soldat2-linuxserver-release.tar.gz" -C ${GAMEDIR} 
+    cp -r soldat2-linuxserver-release/* .
+    rm -rf soldat2-linuxserver-release
+fi
+
+echo "Updating server config"
+
+cp ${TMPDIR}/autoconfig.ini ${GAMEDIR}
+sed -i -e "s|RCON_PASSWORD|${RCON_PASSWORD}|g" \
+        -e "s|SERVER_NAME|${SERVER_NAME}|g" \
+        -e "s|SERVER_GREET_MESSAGE|${SERVER_GREET_MESSAGE}|g" \
+        -e "s|GLOBAL_DEBUG|${GLOBAL_DEBUG}|g" \
+        -e "s|NET_DEBUG|${NET_DEBUG}|g" \
+        "${GAMEDIR}"/autoconfig.ini
+
+echo "Running server"
+exec "$@"
